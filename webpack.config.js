b7ed54bcdbc88d85d3827245c82f2dcc59995ce5
sigmunog <sigmunog@stud.ntnu.no>
2020-06-24 15:11:40 +0200

    more fix for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,11 @@ module.exports = {
     compress: true,
     port: 8080,
     disableHostCheck: true,
+    host: 'blueprint.local.overhang.io',
+    allowedHosts:[
+        'localhost',
+        'blueprint.local.overhang.io',
+        'local.overhang.io',
+    ],
   }
 };
